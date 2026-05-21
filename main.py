5edb8dde956a8670028630e6f1bf546431ff6890
Bangnoi <bangnoi1993@gmail.com>
2026-05-21 18:17:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-import subprocess
+import yt_dlp
 
 url = open("youtube.txt").read().strip()
 
+ydl_opts = {
+    'quiet': True,
+    'nocheckcertificate': True,
+    'format': 'best'
+}
+
 try:
-    result = subprocess.check_output(
-        ["yt-dlp", "-g", url],
-        text=True
-    ).strip()
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
 
-    with open("live.m3u8", "w") as f:
-        f.write(result)
+        stream_url = info.get("url", "")
 
-    print("SUCCESS")
+        with open("live.m3u8", "w") as f:
+            f.write(stream_url)
+
+        print(stream_url)
 
 except Exception as e:
 
     with open("live.m3u8", "w") as f:
-        f.write("#ERROR")
+        f.write(str(e))
 
     print(e)
