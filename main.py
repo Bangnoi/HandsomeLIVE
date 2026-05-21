1a2ed4a6960c4a71dc1105feaf504c5f03d24398
Bangnoi <bangnoi1993@gmail.com>
2026-05-21 18:36:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,29 @@ url = open("youtube.txt").read().strip()
 ydl_opts = {
     'quiet': True,
     'cookiefile': 'cookies.txt',
-    'format': 'best'
+    'extractor_args': {
+        'youtube': {
+            'player_client': ['android']
+        }
+    },
+    'format': 'b'
 }
 
 try:
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=False)
 
-        stream_url = info.get("url", "")
+        stream_url = info.get("url")
+
+        if not stream_url:
+            formats = info.get("formats", [])
+            for f in formats:
+                if f.get("url"):
+                    stream_url = f["url"]
+                    break
 
         with open("live.m3u8", "w") as f:
-            f.write(stream_url)
+            f.write(stream_url or "#NO_URL")
 
         print(stream_url)
 
