47f6592818a630e909cd47d09a75c55e786f28ec
Bangnoi <bangnoi1993@gmail.com>
2026-05-21 19:12:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ url = open("youtube.txt").read().strip()
 cmd = [
     "yt-dlp",
     "--cookies", "cookies.txt",
+    "--user-agent",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122 Safari/537.36",
+    "--extractor-args",
+    "youtube:player_client=android",
+    "--no-cache-dir",
     "-g",
     url
 ]
@@ -13,12 +18,16 @@ try:
     out = subprocess.check_output(cmd).decode().splitlines()
 
     m3u8 = ""
+
     for line in out:
-        if "m3u8" in line or "googlevideo" in line:
+        if "googlevideo" in line or "m3u8" in line:
             m3u8 = line
             break
 
-    open("live.m3u8", "w").write(m3u8)
+    if m3u8:
+        open("live.m3u8", "w").write(m3u8)
+    else:
+        open("live.m3u8", "w").write("NO LINK FOUND")
 
 except Exception as e:
     open("live.m3u8", "w").write(str(e))
